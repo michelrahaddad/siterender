d8a473a20bf301b15ba29643c6489f088ec49b21
michelrahaddad <104462721+michelrahaddad@users.noreply.github.com>
2025-06-22 08:03:50 -0300

    Add files via upload

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,14 +1,14 @@
-import { defineConfig } from "drizzle-kit";
+import type { Config } from "drizzle-kit";
+import * as dotenv from "dotenv";
 
-if (!process.env.DATABASE_URL) {
-  throw new Error("DATABASE_URL, ensure the database is provisioned");
-}
+dotenv.config();
 
-export default defineConfig({
+export default {
+  schema: "./schema.ts",
   out: "./migrations",
-  schema: "./shared/schema.ts",
-  dialect: "postgresql",
+  driver: "pg",
   dbCredentials: {
-    url: process.env.DATABASE_URL,
+    connectionString: process.env.DATABASE_URL!,
   },
-});
+  dialect: "postgresql",
+} satisfies Config;
