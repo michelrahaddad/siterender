41945aab1fbec4d282cc63adf2ca9f673fd38e4d
michelrahaddad <104462721+michelrahaddad@users.noreply.github.com>
2025-06-22 13:22:20 -0300

    Update drizzle.config.ts

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -3,9 +3,10 @@ import type { Config } from "drizzle-kit";
 export default {
   schema: "./shared/schema.ts",
   out: "./drizzle",
+  driver: "postgresql",
   dialect: "postgresql",
-  driver: "pg",
   dbCredentials: {
-    url: "postgresql://cartao_vidah_user:UsxdpiN5sqwycdJCYKWLrjO8GgcQRuqX@dpg-d1bl6o8dl3ps73eqbdeg-a.oregon-postgres.render.com/cartao_vidah",
+    connectionString:
+      "postgresql://cartao_vidah_user:UsxdpiN5sqwycdJCYKWLrjO8GgcQRuqX@dpg-d1bl6o8dl3ps73eqbdeg-a.oregon-postgres.render.com/cartao_vidah",
   },
 } satisfies Config;
